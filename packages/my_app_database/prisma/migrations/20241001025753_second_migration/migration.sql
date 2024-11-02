996d506edea976a75f259e6310723f327f7ddd0c
zeig101 <d.q.burgess99@gmail.com>
2024-11-01 19:58:25 -0700

    latest not working

--- packages/my_app_database/prisma/migrations/20241001025753_second_migration/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE `Ingredient` (
-    `id` VARCHAR(191) NOT NULL,
-    `name` VARCHAR(191) NULL,
-    `description` VARCHAR(191) NULL,
-
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
